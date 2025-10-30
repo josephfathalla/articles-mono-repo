927216e9e1883bfc25bc5812bc8a6be5ca23b8bd
Fidel <fidelsam1992@gmail.com>
2025-10-30 15:53:53 +0300

    initial commit

---
+++ apps/web/src/routes/dashboard.tsx
@@ -0,0 +1,30 @@
+import { authClient } from "@/lib/auth-client";
+import { orpc } from "@/utils/orpc";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Dashboard() {
+	const { data: session, isPending } = authClient.useSession();
+	const navigate = useNavigate();
+
+	const privateData = useQuery(orpc.privateData.queryOptions());
+
+	useEffect(() => {
+		if (!session && !isPending) {
+			navigate("/login");
+		}
+	}, [session, isPending, navigate]);
+
+	if (isPending) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			<p>Welcome {session?.user.name}</p>
+			<p>API: {privateData.data?.message}</p>
+		</div>
+	);
+}
