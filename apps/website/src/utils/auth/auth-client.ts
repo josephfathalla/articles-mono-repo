81da284a975b9bfc5214f1b124d83b98af6efcf1
Fidel <fidelsam1992@gmail.com>
2025-11-26 21:11:23 +0200

    fix: build again

--- apps/website/src/utils/auth/auth-client.ts
+++ apps/website/src/utils/auth/auth-client.ts
@@ -3,6 +3,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_SERVER_URL ?? "",
+  baseURL: "https://backend.boilerplate.determinds.com",
+  // baseURL: import.meta.env.VITE_SERVER_URL ?? "",
   plugins: [inferAdditionalFields<typeof auth>()],
 });
