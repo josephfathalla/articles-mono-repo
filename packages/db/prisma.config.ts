ba5926184c456df0c158b80304fbd9e031148b2c
Fidel <fidelsam1992@gmail.com>
2025-11-26 10:04:18 +0200

    fix: building backend

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,18 +1,5 @@
-import path from "node:path";
-// import dotenv from "dotenv";
 import "dotenv/config";
-// import type { PrismaConfig } from "prisma";
-
-// dotenv.config({
-//   path: "../../apps/server/.env",
-// });
-
-// export default {
-//   schema: path.join("prisma", "schema"),
-//   migrations: {
-//     path: path.join("prisma", "migrations"),
-//   },
-// } satisfies PrismaConfig;
+import path from "node:path";
 
 import { defineConfig, env } from "prisma/config";
 
