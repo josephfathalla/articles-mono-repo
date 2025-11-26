d674fc8d98a2ecef2abeba967712302220889e8a
Fidel <fidelsam1992@gmail.com>
2025-11-26 14:38:01 +0200

    refactor: env variables

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -7,10 +7,8 @@ export const auth = betterAuth<BetterAuthOptions>({
     provider: "postgresql",
   }),
   trustedOrigins: [
-    "http://localhost:3000",
-    "http://localhost:3002",
-    "http://localhost:5173",
-    "http://localhost:4000",
+    process.env.CORS_ORIGIN || "",
+    process.env.CORS_WEBSITE_ORIGIN || "",
   ],
   emailAndPassword: {
     enabled: true,
